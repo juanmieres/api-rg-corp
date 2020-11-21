a271a0541e3939cba657157ffdf6d9ce01ee03f3
Juan Mieres <juan.mieres.s@gmail.com>
2020-11-20 22:31:02 -0300

    commit initial

---
+++ api/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import View
+
+
+class ScraperAPI(View):
+    def get(self, *args, **kwargs):
+        pass
+
+    def post(self, *args, **kwargs):
+        pass
+
+    def put(self, *args, **kwargs):
+        pass
+
+    def delete(self, *args, **kwargs):
+        pass
